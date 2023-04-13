4781174bbf05a9864aa27d1d65a67fc8c4457597
vinayakgarole <vinayakgarole10@gamil.com>
2023-04-13 16:59:42 +0530

    Welcome to Employee Payroll Services Project

---
+++ src/test/java/com/bridgelabz/EmployeePayroll/EmployeePayrollApplicationTests.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.EmployeePayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
